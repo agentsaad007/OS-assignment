4059eff29975c6ff76aaf81fc3c1953873644a50
Saad Ali <agentsaad07@yahoo.com>
2016-05-05 20:15:07 +0500

    Multiple process in SJF at same time sol

--- sjf_multiple_p_on_sametime_sol.py
+++ sjf_multiple_p_on_sametime_sol.py
@@ -1,27 +1,33 @@
 arr = []
-bt=[]
+wt=[]
 wtotal=0
-temp=0
+temp=k=ind=c=0;
 n = int(raw_input('Enter the total no of processes: '))
-ind=[0]*n
 wt=[0]*n
 for i in xrange(n):
     arr.append([])
     arr[i].append(raw_input('Enter arrival time of process '+repr(i+1)+': '))
     arr[i].append(int(raw_input('Enter burst time of process '+repr(i+1)+': ')))
     arr[i].append(i+1)
-j=1
-arr.sort(key = lambda arr:arr[1])
-j=0
-for j in xrange(n)
-	temp=arr[j][0]	
-	for i in xrange(n):
-		if temp>arr[i][0] and arr[i]!=0
-			ind.append(i)
-			temp=arr[i][0]	
-wt[ind]=arr[ind][1]
-wtotal+=wt[ind]
-arr[ind][0]=0
+    arr[i].append(0)
+arr.sort(key = lambda arr:arr[0])
+i=0
+while i<n and c != n:
+	if arr[i][3] != -1:
+		temp=arr[i][1]
+		k=arr[i][0]
+		j=0
+		ind=i
+		for j in xrange(n):
+			if temp>arr[j][1] and arr[j][0]==k and arr[j][3] != -1:				
+				temp=arr[j][1]
+				ind=j
+		wt[ind]=wtotal		
+		wtotal=wtotal+arr[ind][1]
+		arr[ind][3]=-1
+		if ind==i:
+			i+=1
+		n+=1	
 print 'ProcessNumber\tArrivalTime\tBurstTime\tWaiting Time'
 for i in xrange(n):
     print str(arr[i][2]), '\t\t', str(arr[i][0]), '\t\t', str(arr[i][1]), '\t\t', str(wt[i])
